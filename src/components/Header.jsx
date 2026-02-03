89d5d005b44652a31cf3cd85911b5e822ab5e93d
Pavel <yashchenkodev@gmail.com>
2026-02-03 17:57:49 +0200

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ const Header = () => {
     <header className="header">
       <div className="container">
         <h1 className="cv-title">Claudette Lening</h1>
-        <p className="sv-subtitle">
+        <p className="cv-subtitle">
           CareerOS Academy - Barcelona, Spain
           <a href="mailto:claudette.lening@cosacademy.edu"> claudette.lening@cosacademy.edu </a>
-          <a href="tel:+34612345678">+34612 345 678 </a>{' '}
+          <a href="tel:+34612345678">+34612 345 678 </a>
           <a href="linkedin.com/in/claudette-lening">linkedin.com/in/claudette-lening</a>
         </p>
       </div>
