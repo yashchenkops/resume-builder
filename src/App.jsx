6b888cb8f7af0890be8558e84ae7472ba6cdc599
Pavel <yashchenkodev@gmail.com>
2026-02-04 16:05:23 +0200

    feat: finish project

--- src/App.jsx
+++ src/App.jsx
@@ -16,54 +16,56 @@ function App() {
   const { comments, addComment } = useComments();
 
   return (
-    <div className="wrapper">
-      <main className="resume">
-        <Header />
-        <Section title="Relevant Experience">
-          {experience.map((item) => (
-            <SectionItem
-              key={item.id}
-              left={item.date}
-              company={item.company}
-              location={item.location}
-            >
-              <Experience position={item.position} description={item.description} />
-            </SectionItem>
-          ))}
-        </Section>
-        <Section title="Education">
-          {education.map((item) => (
-            <SectionItem
-              key={item.id}
-              left={item.date}
-              company={item.company}
-              location={item.location}
-            >
-              <Experience position={item.position} description={item.description} />
-            </SectionItem>
-          ))}
-        </Section>
-        <Section title="Extracurricular & Awards">
-          {awards.map((item) => (
-            <SectionItem key={item.id} left={item.date}>
-              {item.text}
-            </SectionItem>
-          ))}
-        </Section>
-        <Section title="Additional Information & Skills">
-          {skills.map((skill) => (
-            <SectionItem key={skill.id} left={`${skill.label}:`}>
-              {skill.items.join(', ')}
-            </SectionItem>
-          ))}
-        </Section>
+    <>
+      <div className="wrapper">
+        <main className="resume">
+          <Header />
+          <Section title="Relevant Experience">
+            {experience.map((item) => (
+              <SectionItem
+                key={item.id}
+                left={item.date}
+                company={item.company}
+                location={item.location}
+              >
+                <Experience position={item.position} description={item.description} />
+              </SectionItem>
+            ))}
+          </Section>
+          <Section title="Education">
+            {education.map((item) => (
+              <SectionItem
+                key={item.id}
+                left={item.date}
+                company={item.company}
+                location={item.location}
+              >
+                <Experience position={item.position} description={item.description} />
+              </SectionItem>
+            ))}
+          </Section>
+          <Section title="Extracurricular & Awards">
+            {awards.map((item) => (
+              <SectionItem key={item.id} left={item.date}>
+                {item.text}
+              </SectionItem>
+            ))}
+          </Section>
+          <Section title="Additional Information & Skills">
+            {skills.map((skill) => (
+              <SectionItem key={skill.id} left={`${skill.label}:`}>
+                {skill.items.join(', ')}
+              </SectionItem>
+            ))}
+          </Section>
+        </main>
+
+        {comments.length > 0 && <CommentsSidebar comments={comments} />}
+      </div>
         <CommentsButton onClick={() => setIsModalOpen(true)} />
-      </main>
 
-      {comments.length > 0 && <CommentsSidebar comments={comments} />}
-
-      {isModalOpen && <CommentsModal onClose={() => setIsModalOpen(false)} onSave={addComment} />}
-    </div>
+        {isModalOpen && <CommentsModal onClose={() => setIsModalOpen(false)} onSave={addComment} />}
+    </>
   );
 }
 
